ea29916114c902d53b6b9f6a830da982e47cde4c
ShreyAswal <aswalshrey@gmail.com>
2024-08-27 17:02:03 +0530

    Authentication middleware implementation

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,18 @@ const sequelize = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const cors = require("cors");
 const app = express();
+const authorizeUser = require("./middlewares/authorizeUser");
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/users", userRoutes);
+
+// Test API
+app.get("/test", authorizeUser, (req, res) => {
+  res.status(200).json({ success: true, user: req.user });
+});
 
 sequelize
   .sync()
